bf5105671914355df456cb6ceac46dc857104b0b
aravindh0302 <aravindh0302@gmail.com>
2026-02-18 18:14:27 +0530

    S3 bucket name changed

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "s3" {
   count = var.bucket_count
-  bucket = "${var.envs}-bucket-${count.index}"
+  bucket = "${var.envs}-awsbucket-${count.index}"
 
   tags = {
-    Bucket_Name = "${var.envs}-bucket-${count.index}"
+    Bucket_Name = "${var.envs}-awsbucket-${count.index}"
     Environment = var.envs
   }
 }
