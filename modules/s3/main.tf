73877a0064674c2e263b513de756db5d1bfb9642
aravindh0302 <aravindh0302@gmail.com>
2026-02-18 18:06:17 +0530

    File Modified

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,8 +1,6 @@
 resource "aws_s3_bucket" "s3" {
   count = var.bucket_count
   bucket = "${var.envs}-bucket-${count.index}"
-  region="us-east-1"
-  
 
   tags = {
     Bucket_Name = "${var.envs}-bucket-${count.index}"
