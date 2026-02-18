b84cb5534b23a84804f459ee34cfcbb42109c956
aravindh0302 <aravindh0302@gmail.com>
2026-02-18 18:35:40 +0530

    Destroying the resource

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,4 +1,4 @@
-module "vpc"{
+/*module "vpc"{
     source = "../../modules/vpc"
     cidr_block="10.0.0.0/16"
     subnet-cidr_block = "10.0.1.0/24"
@@ -20,4 +20,4 @@ module "s3"{
     bucket_count = 1
     envs = "dev"
 }
-
+*/
