434deb1ee83d50da7593a9892118d7f09b000963
aravindh0302 <aravindh0302@gmail.com>
2026-02-18 16:33:01 +0530

    Dev files and Jenkins file added

---
+++ environments/dev/main.tf
@@ -0,0 +1,23 @@
+module "vpc"{
+    source = "../../modules/vpc"
+    cidr_block="10.0.0.0/16"
+    subnet-cidr_block = "10.0.1.0/24"
+    subnet-az = "${var.aws-region}a"
+    envs = "dev"
+}
+
+module "ec2"{
+    source = "../../modules/ec2"
+    ami-id = "ami-0b6c6ebed2801a5cb"
+    instance_type = "t3.small"
+    instance_count = 2
+    subnet_id = module.vpc.subnet_id
+    envs = "dev"
+}
+
+module "s3"{
+    source = "../../modules/s3"
+    bucket_count = 1
+    envs = "dev"
+}
+
